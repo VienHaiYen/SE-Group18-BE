6da85e56225c5e85b216c1649fc9aefcec44fdaf
Nguyen Vu <loliisdabezt@gmail.com>
2022-12-06 13:55:20 +0700

    update params

--- student-database-api/params/params.js
+++ student-database-api/params/params.js
@@ -1,7 +1,7 @@
 // Admininistrator's username: admin
 // Admininistrator's password: admin
 
-const DATABASE = "not yet created";
+const DATABASE = "mongodb+srv://admin:<password>@studentcluster.eqxc5r9.mongodb.net/StudentManagement?retryWrites=true&w=majority";
 
 // const OTHER_DATABASE = "well of course not yet created"
 module.exports = {
--- student-database-api/routes/web/index.js
+++ student-database-api/routes/web/index.js
@@ -1,7 +1,6 @@
 var express = require("express");
-
 var router = express.Router()
-
+var auth = require("../../auth/auth");
 
 router.use(function(req, res, next) {
     res.locals.currentUser = req.user;
@@ -9,5 +8,9 @@ router.use(function(req, res, next) {
     res.locals.info = req.flash("info");
     next();
 })
+
+router.use("/admin", require("./admin"));
+router.use("/teacher", require("./teacher"));
+router.use("/student", require("./student"));
 
 module.exports = router;
