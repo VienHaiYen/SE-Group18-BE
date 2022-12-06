c1e599807575e1ea7a3f8c8e9d4d9dcf313293a2
Nguyen Vu <loliisdabezt@gmail.com>
2022-12-06 13:56:39 +0700

    params password

--- student-database-api/params/params.js
+++ student-database-api/params/params.js
@@ -1,7 +1,7 @@
 // Admininistrator's username: admin
 // Admininistrator's password: admin
 
-const DATABASE = "mongodb+srv://admin:<password>@studentcluster.eqxc5r9.mongodb.net/StudentManagement?retryWrites=true&w=majority";
+const DATABASE = "mongodb+srv://admin:admin123@studentcluster.eqxc5r9.mongodb.net/StudentManagement?retryWrites=true&w=majority";
 
 // const OTHER_DATABASE = "well of course not yet created"
 module.exports = {
