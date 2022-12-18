3f6316e0254c08005542499a171fdb0697dbf18f
Nguyen Vu <loliisdabezt@gmail.com>
2022-12-18 15:19:51 +0700

    restore

--- student-database-api/routes/api/index.js
+++ student-database-api/routes/api/index.js
@@ -2,15 +2,7 @@ const express = require("express");
 const { get } = require("mongoose");
 var router = express.Router();
 
-router.get("/viewMoreInfo", function(req, res) {
-    res.json({
-                "Vien Hai Hien" : "MSSV",
-                "Trinh Le Nguyen Vu": "MSSV",
-                "Phuong Linh":"MSSV",
-                "Le Minh Tri":"MSSV",
-                "Thinh Ngo":"MSSV",
-    })
-})
+
 
 
 // Link to other routers here
