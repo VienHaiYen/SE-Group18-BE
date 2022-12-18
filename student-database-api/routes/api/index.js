73b287174b529f530d2cbf6abad2166c14d38e91
Nguyen Vu <loliisdabezt@gmail.com>
2022-12-18 15:22:59 +0700

    install nodemon as Dev dependency

--- student-database-api/routes/api/index.js
+++ student-database-api/routes/api/index.js
@@ -4,4 +4,14 @@ var router = express.Router();
 
 // Link to other routers here
 
+router.get("/viewMoreInfo", function(req, res) {
+    res.json({
+                "Vien Hai Hien" : "MSSV",
+                "Trinh Le Nguyen Vu": "MSSV",
+                "Phuong Linh":"MSSV",
+                "Le Minh Tri":"MSSV",
+                "Thinh Ngo":"MSSV",
+    })
+})
+
 module.exports = router;
