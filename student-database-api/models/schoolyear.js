053158ada7b605a677474efc44170a19545fb114
Phlinh29102 <trlinh291@gmail.com>
2022-12-20 12:38:15 +0700

    create schoolyear schema

---
+++ student-database-api/models/schoolyear.js
@@ -0,0 +1,21 @@
+var mongoose = require("mongoose");
+var schoolyearSchema = new mongoose.Schema({
+    year:{
+        type:String,
+        required:true
+    },
+    semester:{
+        type:Number,
+        enum: [1, 2],
+        required:true
+    },
+    nid:{
+        type:Number,
+        required:true,
+        unique:true
+    }
+});
+
+var SchoolYear = mongoose.model("Schoolyear", schoolyearSchema);
+
+module.exports = SchoolYear
